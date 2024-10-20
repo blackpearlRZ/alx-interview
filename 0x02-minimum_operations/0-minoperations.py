e34fba3f46636c66de76aae31dda569f93a776b2
root <root@DESKTOP-3GNKSHF.>
2024-10-20 16:22:30 +0100

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,12 +8,12 @@ def minOperations(n):
         to create n * 'H' characters . the only allowed operations are:
         copy all & Paste
     """
-    if n<= 1:
+    if n <= 1:
         return 0
     num = n
     divisions = []
     i = 2
-    while  i <= num:
+    while i <= num:
         if num % i == 0:
             divisions.append(i)
             num = num // i
