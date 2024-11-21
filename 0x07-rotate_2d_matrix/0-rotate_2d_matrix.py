0c7a67fa4a3812e7eec98b564ee3f6144f3db600
chaimae <chaimaeaafif@gmail.com>
2024-11-21 23:13:14 +0000

    hi

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,1 +1,19 @@
-hi
+#!/usr/bin/python3
+""" this module rotates a 2D matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """ this function rotates a 2D matrix
+    in clockwise directions.
+    Args:
+        matrix(list): the matrix to rotate
+    Returns:
+        None
+    """
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(len(matrix)):
+        matrix[i].reverse()
+
